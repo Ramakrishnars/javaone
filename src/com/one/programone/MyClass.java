906a4d5814de6d800a6f83ba05ee73931ac3bdb0
Ramakrishnars <bandiramakrishna@gmail.com>
2021-01-01 23:08:52 +0530

    This is javaproject one

---
+++ src/com/one/programone/MyClass.java
@@ -0,0 +1,10 @@
+package com.one.programone;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           System.out.println("welcome to java project");
+	}
+
+}
